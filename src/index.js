897003e935cd0d0e8fa79dbadcb749a75ad26099
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-31 11:49:46 +0300

    add validate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-//import 'bootstrap';
+// import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import '../assets/img/rss.ico';
+import { string } from 'yup';
 
-console.log("Hello World!");
+const model = {
+  rssForm: {
+    state: 'filling',
+    inputValue: '',
+    validationError: '',
+  },
+};
+
+const isValidateUrl = (url) => {
+  const validValues = [model.rssForm.inputValue];
+  const urlScheema = string().url().notOneOf(validValues);
+  return urlScheema.isValid(url);
+};
+
+const form = document.getElementById('rss-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let { state, inputValue, validationError } = model.rssForm;
+
+  const formData = new FormData(e.target).get('url').trim();
+  //разобраться с асинхронностью!!!
+  isValidateUrl(formData)
+    .then((valid) => {
+      //console.log(valid);
+      // if (valid) {
+      //   state = 'filling';
+      // } else {
+      //   state = 'failed';
+      //   validationError = 'Ссылка должна быть валидным URL';
+      // }
+      
+    });
+    inputValue = formData;
+ console.log(state, inputValue, validationError);
+  // console.log(formData);
+});
