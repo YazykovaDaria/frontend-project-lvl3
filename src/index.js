7e50ffb0126bd7decd17e05a336c38c6678102a9
YazykovaDaria <yazykovadasva@gmail.com>
2022-09-04 17:16:51 +0300

    transfer app

--- src/index.js
+++ src/index.js
@@ -1,43 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import i18next from 'i18next';
-import observerModel from './wiev';
-import validateUrl from './utils/validation';
-import locale from './utils/locales';
+import app from './app';
 
-const RSS_FORM = document.getElementById('rss-form');
-const RSS_INPUT = document.querySelector('#url-input');
-
-const model = {
-  rssForm: {
-    state: 'waiting',
-    inputValue: [],
-    validationMessage: '',
-  },
-};
-
-i18next.init(locale);
-
-
-const watchedModel = observerModel(model, i18next);
-
-RSS_FORM.addEventListener('submit', (e) => {
-  e.preventDefault();
-  watchedModel.rssForm.state = 'validation';
-  const url = new FormData(e.target).get('url').trim();
-
-  validateUrl(url, model.rssForm.inputValue, i18next)
-    .then((res) => {
-      watchedModel.rssForm.inputValue = [res];
-      //watchedModel.rssForm.validationMessage = 'RSS успешно загружен'
-      //console.log(res);
-    })
-    .catch((err) => {
-      watchedModel.rssForm.validationMessage = err.message;
-      watchedModel.rssForm.state = 'invalid';
-      console.log(err);
-    });
-});
-
-RSS_INPUT.addEventListener('input', () => {
-  watchedModel.rssForm.state = 'waiting';
-})
+app();
