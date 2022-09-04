d65d8ede216090501559cb45600d9aedb94f8b3a
YazykovaDaria <yazykovadasva@gmail.com>
2022-09-04 22:19:38 +0300

    add renders

--- src/wiev.js
+++ src/wiev.js
@@ -2,24 +2,89 @@ import onChange from 'on-change';
 
 const RSS_FEEDBACK = document.querySelector('.feedback');
 const RSS_BUTTON = document.querySelector('#rss-btn');
-const RSS_INPUT = document.querySelector('#url-input');
+//const RSS_INPUT = document.querySelector('#url-input');
+const FEED_CONTAINER = document.querySelector('.feeds');
+const POSTS_CONTAINER = document.querySelector('.posts');
 
 const toggleDisabled = (bool) => RSS_BUTTON.disabled = bool;
-//(bool ? RSS_BUTTON.setAttribute('disabled', true) : RSS_BUTTON.removeAttribute('disabled', false));
 
-const wievInvalidRss = (validationMessage) => {
-  RSS_FEEDBACK.textContent = validationMessage;
-  RSS_INPUT.classList.add('is-invalid');
-  toggleDisabled(true);
+
+const wievFeedback = (feedbackMessage) => {
+  RSS_FEEDBACK.textContent = feedbackMessage;
+  // RSS_INPUT.classList.add('is-invalid');
+  // toggleDisabled(true);
 };
+
+const rssFormRender = (state, feedback) => {
+ console.log(feedback);
+  switch (state) {
+    case 'invalid':
+      wievFeedback(feedback);
+      break;
+      case 'waiting':
+        //toggleDisabled(false);
+        wievFeedback(feedback);
+        break;
+        case 'validation':
+          //toggleDisabled(true);
+          wievFeedback(feedback);
+          break;
+case 'sending':
+wievFeedback(feedback);
+break;
+
+        default:
+          throw new Error('хз что происходит')
+  }
+}
+
+const feedRender = (feedContent, i18n) => {
+  const feedTitle = document.createElement('h2');
+  feedTitle.textContent = i18n.t('feed');
+  const title = document.createElement('p');
+  title.textContent = feedContent.title;
+  FEED_CONTAINER.setAttribute('id', feedContent.id);
+  const description = document.createElement('p');
+  description.textContent = feedContent.description;
+  FEED_CONTAINER.append(feedTitle);
+  FEED_CONTAINER.append(title);
+  FEED_CONTAINER.append(description);
+};
+
+const postsRender = (posts, i18n) => {
+  const postsTitle = document.createElement('h2');
+  postsTitle.textContent = i18n.t('posts.title');
+  const btnContent = i18n.t('posts.btn');
+  const postsLink = posts.map((post) => {
+    const link = `<div>
+    <a href=${post.link} id=${post.id}>${post.title}</a>
+    <button>${btnContent}</button>
+    </div>`;
+    // document.createElement('a');
+    // link.setAttribute('href', post.link);
+    // link.setAttribute('id', post.id);
+    // link.textContent = post.title;
+    return link;
+  })
+  POSTS_CONTAINER.innerHTML = postsLink.join('');
+POSTS_CONTAINER.prepend(postsTitle);
+  console.log(postsLink);
+}
 
 const observerModel = (model, i18n) => onChange(model, (path, value) => {
   //console.log('value:', value);
-  const { validationMessage } = model.rssForm;
+  const { feedbackMessage } = model.rssForm;
+
   switch (path) {
     case 'rssForm.state':
-      if (value === 'invalid') wievInvalidRss(validationMessage);
-      else if (value === 'waiting') toggleDisabled(false);
+      rssFormRender(value, feedbackMessage);
+      break;
+case 'feed':
+  feedRender(value, i18n);
+break;
+case 'posts':
+  postsRender(value, i18n);
+  break;
 
     default:
       return 'hi';
