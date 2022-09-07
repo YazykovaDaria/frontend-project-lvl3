215fbac5dc2e71b9ebbe21d757c343f94348676c
YazykovaDaria <yazykovadasva@gmail.com>
2022-09-07 16:06:02 +0300

    add elements

--- src/app.js
+++ src/app.js
@@ -1,71 +1,78 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import i18next from 'i18next';
-import observerModel from './wiev';
+import appWiev from './wiev';
 import validateUrl from './utils/validation';
 import locale from './utils/locales';
 import getRssData from './utils/getRssData';
 import parser from './utils/parserRss';
 import addId from './utils/addId';
 
-const RSS_FORM = document.getElementById('rss-form');
-const RSS_INPUT = document.querySelector('#url-input');
 
 const model = {
   rssForm: {
-    state: 'waiting',
+    state: 'filling',
     inputValue: [],
     feedbackMessage: '',
   },
   feed: {},
   posts: [],
+modal: {
+  visible: false,
+  data: [],
+},
+};
 
+const elements = {
+  rssForm: {
+    form: document.getElementById('rss-form'),
+    input: document.querySelector('#url-input'),
+    feedback: document.querySelector('.feedback'),
+    btn: document.querySelector('#rss-btn'),
+  },
+  postsContainer: document.querySelector('.list-group'),
+  feedContainer: document.querySelector('.feeds'),
 };
 
 i18next.init(locale);
-const watchedModel = observerModel(model, i18next);
-//console.log(i18next.t('rssForm.sucsses'));
-const addRss = (link) => {
+const watchedModel = appWiev(model, elements, i18next);
+
+const addFeedAndPosts = (link) => {
   getRssData(link)
     .then((xml) => {
       const rssContent = addId(parser(xml.contents));
-      //watchedModel.feedContent = rssContent;
       watchedModel.feed = rssContent.feed;
       watchedModel.posts = rssContent.posts;
-      watchedModel.rssForm.feedbackMessage = i18next.t('rssForm.sucsses');
-         //console.log(model);
+      model.rssForm.feedbackMessage = i18next.t('rssForm.uploadSucsses');
+      watchedModel.rssForm.state = 'filling';
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-//addRss('https://ru.hexlet.io/lessons.rss')
+//addFeedAndPosts('http://lorem-rss.herokuapp.com/feed?unit=second&interval=10');
 
 const app = () => {
-  RSS_FORM.addEventListener('submit', (e) => {
+  elements.rssForm.form.addEventListener('submit', (e) => {
     e.preventDefault();
     watchedModel.rssForm.state = 'validation';
     const url = new FormData(e.target).get('url').trim();
 
-    watchedModel.rssForm.state = 'sending';
-    watchedModel.rssForm.feedbackMessage = i18next.t('rssForm.upload');
     validateUrl(url, model.rssForm.inputValue, i18next)
       .then((res) => {
-        watchedModel.rssForm.inputValue = [res];
-        addRss(res);
-        // .then((data) => console.log(data))
-        // console.log(rssData);
-      // watchedModel.rssForm.feedbackMessage = 'RSS успешно загружен'
-      // console.log(res);
+        model.rssForm.feedbackMessage = i18next.t('rssForm.uploading');
+        watchedModel.rssForm.state = 'sending';
+        model.rssForm.inputValue = [res];
+        addFeedAndPosts(res);
       })
       .catch((err) => {
-        watchedModel.rssForm.feedbackMessage = err.message;
+        model.rssForm.feedbackMessage = err.message;
         watchedModel.rssForm.state = 'invalid';
-        console.log(err);
+        // console.log(err);
       });
   });
 
-  RSS_INPUT.addEventListener('input', () => {
+  elements.rssForm.input.addEventListener('input', () => {
     watchedModel.rssForm.state = 'waiting';
   });
 };
