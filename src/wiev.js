c978ffb49793d1b0c9aaed612b2af27b26f6f8e8
YazykovaDaria <yazykovadasva@gmail.com>
2022-09-07 12:46:52 +0300

    add scc-classes, disabled

--- src/wiev.js
+++ src/wiev.js
@@ -2,50 +2,48 @@ import onChange from 'on-change';
 
 const RSS_FEEDBACK = document.querySelector('.feedback');
 const RSS_BUTTON = document.querySelector('#rss-btn');
-//const RSS_INPUT = document.querySelector('#url-input');
+// const RSS_INPUT = document.querySelector('#url-input');
 const FEED_CONTAINER = document.querySelector('.feeds');
 const POSTS_CONTAINER = document.querySelector('.posts');
 
 const toggleDisabled = (bool) => RSS_BUTTON.disabled = bool;
 
-
-const wievFeedback = (feedbackMessage) => {
-  RSS_FEEDBACK.textContent = feedbackMessage;
-  // RSS_INPUT.classList.add('is-invalid');
-  // toggleDisabled(true);
-};
-
 const rssFormRender = (state, feedback) => {
- console.log(feedback);
+  RSS_FEEDBACK.textContent = feedback;
   switch (state) {
     case 'invalid':
-      wievFeedback(feedback);
+      RSS_FEEDBACK.setAttribute('class', 'text-danger');
+      toggleDisabled(true);
       break;
-      case 'waiting':
-        //toggleDisabled(false);
-        wievFeedback(feedback);
-        break;
-        case 'validation':
-          //toggleDisabled(true);
-          wievFeedback(feedback);
-          break;
-case 'sending':
-wievFeedback(feedback);
-break;
+    case 'waiting':
+      RSS_FEEDBACK.setAttribute('class', 'text-success');
+      toggleDisabled(false);
+      break;
+    case 'validation':
 
-        default:
-          throw new Error('хз что происходит')
+      RSS_FEEDBACK.setAttribute('class', 'text-danger');
+      toggleDisabled(true);
+      break;
+    case 'sending':
+      RSS_FEEDBACK.setAttribute('class', 'text-warning');
+      toggleDisabled(true);
+      break;
+
+    default:
+      throw new Error('хз что происходит');
   }
-}
+};
 
 const feedRender = (feedContent, i18n) => {
   const feedTitle = document.createElement('h2');
   feedTitle.textContent = i18n.t('feed');
+  feedTitle.setAttribute('class', 'mb-3');
   const title = document.createElement('p');
   title.textContent = feedContent.title;
   FEED_CONTAINER.setAttribute('id', feedContent.id);
   const description = document.createElement('p');
   description.textContent = feedContent.description;
+  description.setAttribute('class', 'text-secondary');
   FEED_CONTAINER.append(feedTitle);
   FEED_CONTAINER.append(title);
   FEED_CONTAINER.append(description);
@@ -55,46 +53,45 @@ const postsRender = (posts, i18n) => {
   const postsTitle = document.createElement('h2');
   postsTitle.textContent = i18n.t('posts.title');
   const btnContent = i18n.t('posts.btn');
+
+  const postsList = document.createElement('ul');
+  postsList.setAttribute('class', 'list-group');
+
   const postsLink = posts.map((post) => {
-    const link = `<div>
+    const link = `<li class="d-flex justify-content-between align-items-center my-2">
     <a href=${post.link} id=${post.id}>${post.title}</a>
-    <button>${btnContent}</button>
-    </div>`;
+    <button class="btn btn-outline-primary">${btnContent}</button>
+    </li>`;
     // document.createElement('a');
     // link.setAttribute('href', post.link);
     // link.setAttribute('id', post.id);
     // link.textContent = post.title;
     return link;
-  })
-  POSTS_CONTAINER.innerHTML = postsLink.join('');
-POSTS_CONTAINER.prepend(postsTitle);
-  console.log(postsLink);
-}
+  });
+  postsList.innerHTML = postsLink.join('');
+  POSTS_CONTAINER.append(postsTitle);
+  POSTS_CONTAINER.append(postsList);
+  // console.log(postsLink);
+};
 
-const observerModel = (model, i18n) => onChange(model, (path, value) => {
-  //console.log('value:', value);
+const appWiev = (model, i18n) => onChange(model, (path, value) => {
+  // console.log(path);
   const { feedbackMessage } = model.rssForm;
 
   switch (path) {
     case 'rssForm.state':
       rssFormRender(value, feedbackMessage);
       break;
-case 'feed':
-  feedRender(value, i18n);
-break;
-case 'posts':
-  postsRender(value, i18n);
-  break;
+    case 'feed':
+      feedRender(value, i18n);
+      break;
+    case 'posts':
+      postsRender(value, i18n);
+      break;
 
     default:
-      return 'hi';
+      throw new Error('unknow path');
   }
-  // console.log('Object changed:', ++index);
-  // console.log('this:', this);
-  // console.log('path:', path);
-  //console.log('value:', value);
-  // console.log('previousValue:', previousValue);
-  // console.log('applyData:', applyData);
 });
 
-export default observerModel;
+export default appWiev;
