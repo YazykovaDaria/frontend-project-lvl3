44e54324094fce2b709f496ac52f402d80ee3729
YazykovaDaria <yazykovadasva@gmail.com>
2022-09-08 11:39:59 +0300

    rename

--- src/utils/addId.js
+++
@@ -1,9 +0,0 @@
-import { uniqueId } from 'lodash';
-
-const addId = (rssData) => {
-  const posts = rssData.posts.map((post) => ({ ...post, id: uniqueId('post') }));
-  const feed = { ...rssData.feed, id: uniqueId('feed') };
-  return { feed, posts };
-};
-
-export default addId;
